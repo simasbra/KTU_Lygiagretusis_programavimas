7d3b1c33262795dbf36661aae49ae29321adc8ff
Simas Bradaitis <simasbra@proton.me>
2024-09-05 08:18:10 +0300

    cJSON

--- Laboratorinis1/src/dataGenerator.c
+++ Laboratorinis1/src/dataGenerator.c
@@ -1,6 +1,20 @@
 #include <stdlib.h>
+#include "cjson/cJSON.h"
+
+void fill_array(cJSON *);
 
 int main(void) {
+	cJSON *pUsers = cJSON_CreateArray();
 
+	cJSON_free(pUsers);
 	return 0;
+}
+
+void fill_array(cJSON *pUsers) {
+	const int size = 25;
+	char *names[] = { "Jonas", "Petras", "Antanas", "Juozas", "Stasys" };
+
+	for (int i = 0; i < size; i++) {
+
+	}
 }
