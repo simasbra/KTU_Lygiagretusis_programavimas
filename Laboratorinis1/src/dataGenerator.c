d2fe9f73a2240745d01ff5130e9eb4e8d8e8a5ab
Simas Bradaitis <simasbra@proton.me>
2024-09-09 18:51:28 +0300

    data

--- Laboratorinis1/src/dataGenerator.c
+++ Laboratorinis1/src/dataGenerator.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
+// https://github.com/DaveGamble/cJSON
 #include "cjson/cJSON.h"
 
 void fill_array(cJSON *);
@@ -6,16 +8,36 @@ void fill_array(cJSON *);
 int main(void) {
 	cJSON *pUsers = cJSON_CreateArray();
 
+	fill_array(pUsers);
+
+	FILE *pFile = fopen("./data/Simas_BradaitisV_L1_dat_1.json", "w");
+	if (pFile == NULL) {
+		printf("Error opening the file");
+		return 1;
+	}
+
+	char *pJsonData = cJSON_Print(pUsers);
+	fprintf(pFile, "%s", pJsonData);
+
 	cJSON_free(pUsers);
+	fclose(pFile);
+
 	return 0;
 }
 
 void fill_array(cJSON *pUsers) {
-	const int size = 25;
-
+	const int size = 100;
 	char *names[] = { "Jonas", "Petras", "Antanas", "Juozas", "Stasys" };
 
 	for (int i = 0; i < size; i++) {
-		cJSON_AddStringToObject(pUsers, "name", names[i % sizeof(names) / sizeof(names[i])]);
+		cJSON *pUser = cJSON_CreateObject();
+		int year = 1990 + i % 30;
+		double dayMonth = (1 + i % 31) + ((1 + i % 12) / 100.0);
+
+		cJSON_AddStringToObject(pUser, "name", names[i % (sizeof(names) / sizeof(names[0]))]);
+		cJSON_AddNumberToObject(pUser, "year", year);
+		cJSON_AddNumberToObject(pUser, "dayMonth", dayMonth);
+
+		cJSON_AddItemToArray(pUsers, pUser);
 	}
 }
