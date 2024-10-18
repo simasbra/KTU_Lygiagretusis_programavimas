6cf82fac3748ebdbea2a722053cc97df5a233665
Simas Bradaitis <simasbra@proton.me>
2024-10-18 22:27:28 +0300

    reverse string function

--- Laboratorinis2/src/userResult.c
+++ Laboratorinis2/src/userResult.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <regex.h>
 #include <unistd.h>
+#include <string.h>
 
 void UR_generate_string(UserResult *pUserResult, char *pOutput[]) {
 	if (pUserResult == NULL) {
@@ -13,7 +14,10 @@ void UR_generate_string(UserResult *pUserResult, char *pOutput[]) {
 
 	User userTemporary = pUserResult->user;
 	int multiplication = userTemporary.year * userTemporary.dayMonth;
-	int messageLenght = (int)((ceil(log10(multiplication))) * sizeof(char)) + userTemporary.nameLength;
+	char nameReversed[userTemporary.nameLength];
+	strcpy(nameReversed, userTemporary.name);
+	string_inplace_reverse(&nameReversed);
+	int messageLenght = (int)((ceil(log10(multiplication))) * sizeof(char)) + userTemporary.nameLength * 2 + 1;
 	char message[messageLenght];
 	sprintf(message, "%s%d", userTemporary.name, multiplication);
 
@@ -47,7 +51,6 @@ void UR_generate_string(UserResult *pUserResult, char *pOutput[]) {
 		previous = c | message[i % messageLenght];
 	}
 	*pOutput = result;
-
 }
 
 void	UR_hash_using_sha256(char *pMessage[], char *pHash[]);
@@ -77,4 +80,25 @@ void UR_print_user_result_to_file(UserResult *pUserResult, FILE *pFile) {
 	}
 	fprintf(pFile, "| %-20s | %10d | %10.2lf | %-128s |\n",
 		pUserResult->user.name, pUserResult->user.year, pUserResult->user.dayMonth, pUserResult->hash);
+}
+
+void string_inplace_reverse(char *pString) {
+	if (!pString) {
+		return;
+	}
+	char *pEnd = pString + strlen(pString) + 1;
+	// swap values in two given variables
+	#define XOR_SWAP(a, b) do\
+	{\
+		a ^= b;\
+		b ^= a;\
+		a ^= b;\
+	} while(0);
+	// walk inwards from both ends of the string, swapping until the middle
+	while (pString < pEnd) {
+		XOR_SWAP(*pString, *pEnd);
+		pString++;
+		pEnd--;
+	}
+	#undef XOR_SWAP
 }
--- Laboratorinis2/src/userResult.h
+++ Laboratorinis2/src/userResult.h
@@ -9,12 +9,13 @@ typedef struct UserResult {
 	User	user;
 } UserResult;
 
+void	UR_generate_string(UserResult *pUserResult, char *pOutput[]);
 void	UR_hash_using_sha256(char *pMessage[], char *pHash[]);
 void	UR_hash_using_blake2b(char *pMessage[], char *pHash[]);
-void	UR_generate_string(UserResult *pUserResult, char *pOutput[]);
 int	UR_check_has_ends_with_a_number(UserResult *pUserResult);
 void	UR_print_user_result(UserResult *pUserResult);
 void	UR_print_user_result_to_file(UserResult *pUserResult, FILE *pFile);
 
-#endif
+void	string_inplace_reverse(char *pString);
 
+#endif
